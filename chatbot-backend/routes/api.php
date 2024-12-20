965b43fcbae6af8be00fad61f9cdb62febcd4475
Nemanja Filipovic <nemanja.filipovic000@gmail.com>
2024-12-20 02:29:10 +0100

    Dodate rute

---
+++ chatbot-backend/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatbotController;
+use App\Http\Controllers\ChatHistoryController;
+use App\Http\Controllers\UserController;
+
+Route::post('/register', [AuthController::class, 'register']); 
+Route::post('/login', [AuthController::class, 'login']); 
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::patch('/chatbots/{id}/version', [ChatbotController::class, 'updateVersion']); 
+    Route::put('/chatbots/{id}', [ChatbotController::class, 'updateChatbot']);
+
+    Route::post('/chat-history', [ChatHistoryController::class, 'create']); 
+    Route::delete('/chat-history/{id}', [ChatHistoryController::class, 'destroy']);
+
+    Route::resource('users', UserController::class)->only(['index', 'show', 'destroy']);
+
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+    
